cf14c138ae9dbf6bf5c32da245e05f1c88a9cc19
justyeh <yewenxiang@300.cn>
2019-01-05 17:29:28 +0800

    code

--- utils/database.js
+++ utils/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2/promise');
 let pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'mysql123456',
+    password: '123456',
     port: 3306,
-    database: 'justyeh'
+    database: 'justyeh_temp'
 });
 
 exports.query = async (sql, params) => {
