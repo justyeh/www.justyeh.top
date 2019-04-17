a9c70d143c9131c4649a20737d690a3f32036e9a
justyeh <yewenxiang@300.cn>
2019-04-17 14:26:26 +0800

    update pkg

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var history = require('connect-history-api-fallback');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
 var app = express();
 
-//部署react单页应用
-app.use(history({
-    rewrites: [
-      { from: /^\/admin$/, to: '/' }
-    ]
-  }))
 
 // 设置模板引擎
-var ejs = require('ejs'); 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');+
-app.engine('html', ejs.__express);
+var ejs = require("ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
++app.engine("html", ejs.__express);
 
 // 设置favicon.ico
-app.use(favicon(path.join(__dirname, './static/favicon.ico')));
+app.use(favicon(path.join(__dirname, "./static/favicon.ico")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //设置静态文件目录
-app.use(express.static(path.join(__dirname, './public'))); //html,pdf,project
-app.use(express.static(path.join(__dirname, './static'))); //image,css,js
-app.use(express.static(path.join(__dirname, './uploads'))); //image upload dir
+app.use(express.static(path.join(__dirname, "./public"))); //html,pdf,project
+app.use(express.static(path.join(__dirname, "./static"))); //image,css,js
+app.use(express.static(path.join(__dirname, "./uploads"))); //image upload dir
 
 //session中间件
-app.use(session({
-    secret: 'justyeh.top',
-    resave: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } //设置一天有效期
-}));
-
+app.use(
+    session({
+        secret: "justyeh.top",
+        resave: true,
+        saveUninitialized: true,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 } //设置一天有效期
+    })
+);
 
 //路由
-var front = require('./routes/front');
-var admin = require('./routes/admin');
-var api = require('./routes/api');
+var front = require("./routes/front");
+var admin = require("./routes/admin");
+var api = require("./routes/api");
 
-
-app.use('/', front);
-app.use('/admin', admin);
-app.use('/api', api);
+app.use("/", front);
+app.use("/admin", admin);
+app.use("/api", api);
 
 // 404
 app.use(function(req, res, next) {
-    var err = new Error('This page could not be found.');
+    var err = new Error("This page could not be found.");
     err.status = 404;
     next(err);
 });
@@ -63,9 +56,9 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
     // render the error page
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
-        code: req.app.get('env') === 'development' ? err.status : null,
+        code: req.app.get("env") === "development" ? err.status : null
     });
 });
 
